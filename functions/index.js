586d33a820621c268000010e1f7ff29105dc4017
Jian Shen <jianshen.image.visual@gmail.com>
2026-04-23 04:47:39 +0800

    Fix server.js path for Cloud Functions

--- functions/index.js
+++ functions/index.js
@@ -9,14 +9,19 @@ app.use(express.static(path.join(__dirname, '.next/standalone/public')));
 app.use('/_next', express.static(path.join(__dirname, '.next/standalone/.next')));
 
 // Load and run the Next.js server
-const { default: nextApp } = require(path.join(__dirname, '.next/standalone/server.js'));
+let nextApp;
+try {
+  nextApp = require(path.join(__dirname, './server.js'));
+} catch (err) {
+  console.error('Error loading server:', err);
+}
 
 // Handle all requests
 app.all('*', (req, res) => {
-  if (nextApp) {
+  if (nextApp && typeof nextApp === 'function') {
     return nextApp(req, res);
   }
-  res.status(500).send('Server not ready');
+  res.status(500).send('Server not initialized');
 });
 
 // Export as Cloud Function
